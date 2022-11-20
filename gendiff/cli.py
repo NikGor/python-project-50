458c9ff5c3521c75dbbf3a350bfdf452c865912a
Nikolai Gordienko <nicolas.gordienko@gmail.com>
2022-11-20 23:46:25 +0100

    step 2

---
+++ gendiff/cli.py
@@ -0,0 +1,11 @@
+# command line interface
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('--f', '--format',
+                        help='set format of output')
+    return parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-# gendiff -h
-# usage: gendiff [-h] first_file second_file
-#
-# Compares two configuration files and shows a difference.
-#
-# positional arguments:
-#   first_file
-#   second_file
-#
-# optional arguments:
-#   -h, --help            show this help message and exit
-
-
-import argparse
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-parser.add_argument('first_file', type=str)
-parser.add_argument('second_file', type=str)
-args = parser.parse_args()
+from gendiff.cli import parse_args
 
 
 def main():
-    pass
+    args = parse_args()
 
 
 if __name__ == '__main__':
