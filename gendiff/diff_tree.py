eb607ee3a73cc3e42d1798488c2dbf3e4eb61034
Nikolai Gordienko <nicolas.gordienko@gmail.com>
2022-12-20 23:48:12 +0100

    fixed mentor comments #2

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -2,22 +2,16 @@ def build_diff_tree(old_data, new_data):
     result = {}
     for key, value in old_data.items():
         if key in new_data:
-            # Key is present in both dictionaries
             if isinstance(value, dict) and isinstance(new_data[key], dict):
-                # Nested vertex
-                descendants = build_diff_tree(value, new_data[key])
-                result[key] = {'type': 'nested', 'value': descendants}
+                nodes = build_diff_tree(value, new_data[key])
+                result[key] = {'type': 'nested', 'value': nodes}
             elif value == new_data[key]:
-                # Unchanged vertex
                 result[key] = {'type': 'unchanged', 'value': value}
             else:
-                # Changed vertex
                 result[key] = {'type': 'changed', 'value': {'old': value, 'new': new_data[key]}}
         else:
-            # Removed vertex
             result[key] = {'type': 'removed', 'value': value}
     for key, value in new_data.items():
         if key not in old_data:
-            # Added vertex
             result[key] = {'type': 'added', 'value': value}
     return result
